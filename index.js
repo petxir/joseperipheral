82015f4c50d060ea80e1b24074180022a3d76c97
petxir <108912427+petxir@users.noreply.github.com>
2023-05-09 07:50:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { exec } = require("child_process");
+const path = require('path');
+const app = express()
+const port = 3000
+app.use(express.urlencoded({ extended: true }))
+app.get("/simpan",(req, res) => {
+    const { width, height } = req.query
+    exec("vncserver -geometry "+width+"x"+(height-23))
+    return res.send("jos")
+})
+app.get("/",(req, res) => {
+    return res.sendFile(path.join(__dirname+'/tampilan.html'))
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
