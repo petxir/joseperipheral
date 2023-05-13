585e43c598ba7c97f3f9c3b114c2bf67834b8f67
petxir <108912427+petxir@users.noreply.github.com>
2023-05-13 11:50:34 +0700

    Update perintah.sh

--- perintah.sh
+++ perintah.sh
@@ -9,10 +9,7 @@ wget https://raw.githubusercontent.com/petxir/joseperipheral/main/index.js
 wget https://raw.githubusercontent.com/petxir/joseperipheral/main/vnc.zip
 wget https://raw.githubusercontent.com/petxir/joseperipheral/main/tampilan.html
 wget https://raw.githubusercontent.com/petxir/joseperipheral/main/vncmodip.zip
-#memindah novnc modif
 unzip vncmodip.zip
-sudo rm -rf /usr/share/vnc
-sudo mv novnc /usr/share/
 unzip vnc.zip
 sudo mv .vnc /home/gitpod/
 wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/112.0.2/linux-x86_64/en-US/firefox-112.0.2.tar.bz2
@@ -20,5 +17,5 @@ tar xjf firefox-*.tar.bz2
 sudo mv firefox /opt
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
-websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem  --key=/etc/ssl/novnc.pem  6969 localhost:5901
+websockify -D --web=novnc/ --cert=/etc/ssl/novnc.pem  --key=/etc/ssl/novnc.pem  6969 localhost:5901
 node index.js
