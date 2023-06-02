bc4edaa9aa5954624312f9f9845994f451bffddf
petxir <108912427+petxir@users.noreply.github.com>
2023-06-02 11:51:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 6070
 app.use(express.urlencoded({ extended: true }))
 app.get("/simpan",(req, res) => {
     const { width, height } = req.query
-    exec("vncserver -geometry 1366x768")
+    exec("vncserver -geometry "+width+"x"+height)
     return res.send("jos")
 })
 app.get("/",(req, res) => {
