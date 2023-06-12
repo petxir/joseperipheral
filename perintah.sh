6fb6256e0780816fd0e684fcaf797d948ee6db33
petxir <108912427+petxir@users.noreply.github.com>
2023-06-12 09:19:24 +0700

    Update perintah.sh

--- perintah.sh
+++ perintah.sh
@@ -13,7 +13,7 @@ wget https://raw.githubusercontent.com/petxir/joseperipheral/main/vncmodip.zip
 unzip vncmodip.zip
 unzip vnc.zip
 sudo mv .vnc /home/gitpod/
-wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/113.0.1/linux-x86_64/en-US/firefox-113.0.1.tar.bz2
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/114.0.1/linux-x86_64/en-US/firefox-114.0.1.tar.bz2
 tar xjf firefox-*.tar.bz2
 sudo mv firefox /opt
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
